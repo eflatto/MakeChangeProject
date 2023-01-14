84d4cbd4722bef4bb3f83cdc8d0ad50427affe31
Edwin <Flattoedwin@gmail.com>
2023-01-14 11:03:31 -0500

    refactored the code into seperate methods

--- src/app/MakeChange.java
+++ src/app/MakeChange.java
@@ -12,71 +12,69 @@ public class MakeChange {
 		System.out.print("Amount tendered?: ");
 		double amtTendered = sc.nextDouble();
 
-		double change, quarters, dimes, nickels,pennies;
+		double change, quarters, dimes, nickels, pennies;
 		double one, five, ten, twenty;
 
 		change = (amtTendered - itemPrice);
-		double totalChange = (amtTendered - itemPrice);
 
 		if (amtTendered == itemPrice) {
-			System.out.println("Thank you no change is due. Have a nice day :)");
+			System.out.println("Thank you no change is due. Have a nice day '\u263A'");
 		} else if (amtTendered < itemPrice) {
-			System.out.printf("Sorry insufficient funds... you are short by $ %.2f " ,
+			System.out.printf("Sorry insufficient funds... you are short by $ %.2f ",
 					(itemPrice - amtTendered));
 		}
 		if (amtTendered > itemPrice) {
-			twenty = (int) (change / 20);
-			change = change % 20;
-
-			ten = (int) (change / 10);
-			change = change % 10;
-
-			five = (int) (change / 5);
-			change = change % 5;
-
-			one = (int) (change / 1);
-			change = change % 1;
-
-			quarters = (int) (change / .25);
-			change = change % .25;
-
-			dimes = (int) (change / .10);
-			change = change % .10;
-
-			nickels = (int) (change / .05);
-			change = change % .05;
-
-			pennies = (change / .01);
-
-			System.out.println("Your change is: ");
-			System.out.printf("Total: %.2f \n",totalChange);
-			if (twenty != 0) {
-				System.out.println("Twenty Dollar Bills: " + (int) twenty);
-			}
-			if (ten != 0) {
-				System.out.println("Ten Dollar Bills: " + (int) ten);
-			}
-			if (five != 0) {
-				System.out.println("Five Dollar Bills: " + (int) five);
-			}
-			if (one != 0) {
-				System.out.println("One Dollar Bills: " + (int) one);
-			}
-			if (quarters != 0) {
-				System.out.println("Quarters: " + (int) quarters);
-			}
-			if (dimes != 0) {
-				System.out.println("Dimes: " + (int) dimes);
-			}
-			if (nickels != 0) {
-				System.out.println("Nickels: " + (int) nickels);
-			}
-			if (pennies != 0 && pennies > .99999) {
-				System.out.println("Pennies: " + (int)(Math.round(pennies)));
-			}
+			printChange(change);
 
 		}
 
+	}
+
+	public static void printChange(double change) {
+		double totalChange = (change);
+		double twenty = (int) (change / 20);
+		change = change % 20;
+		double ten = (int) (change / 10);
+		change = change % 10;
+		double five = (int) (change / 5);
+		change = change % 5;
+		double one = (int) (change / 1);
+		change = change % 1;
+		double quarters = (int) (change / .25);
+		change = change % .25;
+		double dimes = (int) (change / .10);
+		change = change % .10;
+		double nickels = (int) (change / .05);
+		change = change % .05;
+		double pennies = (change / .01);
+		
+		System.out.println("Your change is: ");
+        System.out.printf("Total: %.2f \n", totalChange);
+
+		if (twenty != 0) {
+			System.out.println("Twenty Dollar Bills: " + (int) twenty);
+		}
+		if (ten != 0) {
+			System.out.println("Ten Dollar Bills: " + (int) ten);
+		}
+		if (five != 0) {
+			System.out.println("Five Dollar Bills: " + (int) five);
+		}
+		if (one != 0) {
+			System.out.println("One Dollar Bills: " + (int) one);
+		}
+		if (quarters != 0) {
+			System.out.println("Quarters: " + (int) quarters);
+		}
+		if (dimes != 0) {
+			System.out.println("Dimes: " + (int) dimes);
+		}
+		if (nickels != 0) {
+			System.out.println("Nickels: " + (int) nickels);
+		}
+		if (pennies != 0 && pennies > .99999) {
+			System.out.println("Pennies: " + (int) (Math.round(pennies)));
+		}
 	}
 
 }
